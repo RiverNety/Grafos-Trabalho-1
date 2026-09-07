5cc9d78c642f95eb8bec5307e161ab0dbead7606
RiverNety <matheus.leandro219@gmail.com>
2026-09-07 19:22:34 -0300

    adicionado print da matriz de adj, lista de adj e dfs recursiva

--- grafos.cpp
+++ grafos.cpp
@@ -8,6 +8,8 @@ using namespace std;
 vector<vector<int>> vetor_adj;
 vector<vector<int>> matriz_adj;
 int vertices;
+vector<bool> visitados;
+
 
 void ler_grafo(string arquivo){
     ifstream grafo_txt(arquivo);
@@ -19,6 +21,7 @@ void ler_grafo(string arquivo){
     vertices = stoi(linha);
     matriz_adj.resize(vertices + 1, vector<int>(vertices + 1, 0));
     vetor_adj.resize(vertices + 1);
+    visitados.resize(vertices + 1, false)
     
     while(getline(grafo_txt, linha)){
         int u, v;
@@ -33,6 +36,38 @@ void ler_grafo(string arquivo){
 
 }
 
+void imprimir_adj(){
+    for (int i = 1; i <= vertices; i++){
+        cout << i << " -> ";
+        for (int j = 0; j < vetor_adj[i].size(); j++){
+            cout << vetor_adj[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+}
+
+//como colocar os indices?
+void imprimir_matriz(){
+    for (int i = 1; i <= vertices; i++){
+        for (int j = 1; j <= vertices; j++){
+            cout << matriz_adj[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+//dfs recursiva
+void dfs(int v){
+    visitados[v] = true;
+    for (int u : vetor_adj[v]){
+        if (!visitados[u]){
+            dfs(u);
+        }
+    }
+}
+
 int main(){
     ler_grafo("teste.txt");
+    imprimir_matriz();
 }
