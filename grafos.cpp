962c0d99d32e58713505e734167a13b52ad7f36a
RiverNety <matheus.leandro219@gmail.com>
2026-09-14 11:16:54 -0300

    adicionado bfs, impressao das distancias e funcoes auxiliares

--- grafos.cpp
+++ grafos.cpp
@@ -2,15 +2,19 @@
 #include <vector>
 #include <fstream>
 #include <cstdio>
+#include <queue>
 
 using namespace std;
 
+
+int vertices;
 vector<vector<int>> vetor_adj;
 vector<vector<int>> matriz_adj;
-int vertices;
 vector<bool> visitados;
+vector<int> pai;
+vector<int> distancia;
 
-
+//reinicia quando le um arquivo novo
 void ler_grafo(string arquivo){
     ifstream grafo_txt(arquivo);
     string linha;
@@ -20,9 +24,10 @@ void ler_grafo(string arquivo){
     //ajuste de tamanho
     vertices = stoi(linha);
     matriz_adj.resize(vertices + 1, vector<int>(vertices + 1, 0));
-    vetor_adj.resize(vertices + 1);
-    visitados.resize(vertices + 1, false)
-    
+    vetor_adj.resize(vertices + 1, vector<int>(0));
+    visitados.resize(vertices + 1, false);
+    pai.resize(vertices + 1, -1);
+    distancia.resize(vertices + 1, -1);  
     while(getline(grafo_txt, linha)){
         int u, v;
         sscanf(linha.c_str(), "%d %d", &u, &v);
@@ -34,6 +39,23 @@ void ler_grafo(string arquivo){
         matriz_adj[v][u] = 1;
     }
 
+}
+
+void restart_distancia(){
+    for (int i = 1; i <= vertices; i++){
+        distancia[i] = -1;
+    }
+}
+void restart_visitados(){
+    for (int i = 1; i <= vertices; i++){
+        visitados[i] = false;
+    }
+}
+
+void restart_pai(){
+    for (int i = 1; i <= vertices; i++){
+        pai[i] = -1;
+    }
 }
 
 void imprimir_adj(){
@@ -67,7 +89,41 @@ void dfs(int v){
     }
 }
 
+void bfs(int v){
+
+    restart_distancia();
+    restart_visitados();
+    restart_pai();
+
+    queue<int> fila;
+    visitados[v] = true;
+    fila.push(v);
+    pai[v] = -1;
+    distancia[v] = 0;
+    while (!fila.empty()){
+        v = fila.front();
+        fila.pop();
+        for (int u : vetor_adj[v]){
+            if (!visitados[u]){
+                fila.push(u);
+                visitados[u] = true;
+                pai[u] = v;
+                distancia[u] = distancia[v] + 1;
+            }
+        }
+    }
+
+}
+
+void imprimir_distancia(int v){
+    cout << "Distancia de " << v << " ate" << endl;
+    for (int i = 1; i <= vertices; i++){
+        cout << i << " -> " << distancia[i] << endl;
+    }
+}
 int main(){
     ler_grafo("teste.txt");
     imprimir_matriz();
+    bfs(1);
+    imprimir_distancia(1);
 }
